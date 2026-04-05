37268078d0aed307cf9138e4f51e505c451e3528
Tristan Brewster <tristan@ir0k.com>
2026-04-05 12:38:39 -0400

    Initial Commit

---
+++ sharkfin/sharkfinApp.swift
@@ -0,0 +1,17 @@
+//
+//  sharkfinApp.swift
+//  sharkfin
+//
+//  Created by Tristan on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct sharkfinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
